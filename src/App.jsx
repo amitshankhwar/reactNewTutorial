2ac1edaeba29d1a77466e6adcdf3bf35f57554a7
amitshankhwar <amit18shankhwar@gmail.com>
2025-01-07 13:41:09 +0530

    react 1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+//functional components
+
+//class based components
+
+import react from "react";
+import Navbar from "./pages/Navbar";
+import About from "./pages/About";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <About />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(<App />);
+
+//virtual dom -> tree
+//tree-> real dom
+
+//components -> data, files
